077e09ab3008a54718d81042b0396b6f1dc098c3
Aleksej <samarin113@gmail.com>
2019-03-07 23:27:48 +0300

    Codes, comments

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import socket
 from select import select
 
-observables = []
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('127.0.0.1', 1234))
-server_socket.listen()
-
+observables = [] # отслеживаемые сокеты, готовые для чтения
 
 def accept_connection(server_socket):
-    client_socket, address = server_socket.accept()
+    client_socket, address = server_socket.accept() # формирование подключения
     observables.append(client_socket)
     print("Connection from", address)
 
@@ -18,8 +12,8 @@ def accept_connection(server_socket):
 def send_message(client_socket):
     request = client_socket.recv(1024)
     if request:
-        response = (request.decode() + ', hello from server').encode()
-        print(request.decode())
+        response = (request.decode('ansi') + ' - changed to ASCII by server').encode('ascii', 'ignore') # изменение кодировки ответа
+        print(f"Message received: {request.decode('ansi')}")
         client_socket.send(response)
     else:
         client_socket.close()
@@ -28,17 +22,21 @@ def send_message(client_socket):
 def event_loop():
     while True:
         for socket in observables:
-            if socket.fileno() < 0:
+            if socket.fileno() < 0: # если файловый дескриптор отрицательный, то клиент отключился
                 observables.remove(socket)
                 print('Client disconnected')
-        ready_to_read, _, _ = select(observables, [], [])
+        ready_to_read, _, _ = select(observables, [], []) # отслеживание сокетов, готовых для чтения
         for socket in ready_to_read:
             if socket is server_socket:
-                accept_connection(socket)
+                accept_connection(socket) # подключение нового клиента
             else:
-                send_message(socket)
+                send_message(socket) # отправка сообщения клиенту
 
 
 if __name__ == '__main__':
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # настройка серверного сокета
+    server_socket.bind(('127.0.0.1', 1234))
+    server_socket.listen()
+    print(f"Server running on {server_socket.getsockname()}")
     observables.append(server_socket)
     event_loop()
